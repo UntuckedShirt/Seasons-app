55c1cafebda1a59ed990794e92e351d9d6da6658
UntuckedShirt <75454794+UntuckedShirt@users.noreply.github.com>
2021-09-01 14:37:31 -0400

    SeasonDisplay

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>Season Display</div>
+// anytime prop is passed into a functional component it shows up 
+// inside of an an obj that is the first arguement of the functional component
+// referesed to as props
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    
+
+    return <div>{ }</div>
     
 
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 // const in the App component
 // When class is created we are cerating a new class in js that has
 // one method assigned that is the render method
@@ -14,37 +15,60 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     // a constructor is called first anytime a instance of class is created
     // we call constructor to initialze state function
-    constructor(props) {
-        //super is a refrence to the parents constructor function
-        super(props);
-        // THIS IS THE ONLY TIME we do direct assignment
-        //to this.state
-        this.state = { lat: null, errorMessage: ''};
-         //calling back to geo location API
-         window.navigator.geolocation.getCurrentPosition(
+    // constructor only job is to initialzie/update state object
+    // constructor(props) {
+    //     //super is a refrence to the parents constructor function
+    //     super(props);
+    //     // THIS IS THE ONLY TIME we do direct assignment
+    //     //to this.state
+    //     this.state = { lat: null, errorMessage: ''};
+    //      //calling back to geo location API
+         
+    // }
+
+        // the single line statement below is equivelant to the above contructor
+     state = { lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        // componentDidMount allows us to execute the React code when the 
+        // component is already placed in the DOM
+        // if function is defined inside of a class
+        // this function will be auto called once when component first is rendered
+        window.navigator.geolocation.getCurrentPosition(
             //to get reposnse or nformation we need fucntion callback x2
-             position => {
-                 this.setState({ lat: position.coords.latitude });
+             position => this.setState({ lat: position.coords.latitude }),
                  // we did not and do not do this
                  //this.state.lat = postion.cords.latitude
-             }, //first callback/argument known as success callback
-             err => {
-                 this.setState({ errorMessage: err.message });
-            }// second callback/argument for error
+                 //first callback/argument known as success callback
+             err => this.setState({ errorMessage: err.message })
+                // second callback/argument for error
         );
+        
     }
 
+    // componentDidUpdate() {
+    //     // update comes in the form of claling a setState method
+    //     // anytime this component is called rendered will be called first
+    //     console.log("My component did update")
+    // }
+
     // react says we have to define render!!
+    // render is not optional and needs to be defined
+    // render is part of the lifecycle method
     render() {
             //remember to remove semecolons when going from single to multiline statement
             // to conditionally return JSX we need to add simple if statemnts
             // below is called conidtional rendering
+            // render method alone is about returning JSX and nothing
         if (this.state.errorMessage && !this.state.lat) {
           return <div>Error: {this.state.errorMessage}</div>;
         }
     
         if (!this.state.errorMessage && this.state.lat) {
-          return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
+            // wea re taking prop from state of app component and passing
+            // as prop down into the SeasonDisplay
+            // we can take state from one component as a prop down to the child
         }
     
         return <div>Loading...!</div>;
